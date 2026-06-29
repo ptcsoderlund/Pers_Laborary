41d16b2a7435c7ec915029a7779c8cb74fb6b001
PerSöderlund <per.soderlund@plyfa.se>
2026-06-29 19:04:57 +0200

    Starting to think on how to do ECS like storage. Starting with Entities, for now.

---
+++ pl/include/sys/ecs/pl_entity.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "sys/pl_error.h"
+#include "sys/pl_types.h"
+
+typedef u64 pl_entity;
---
+++ pl/src/sys/ecs/pl_entity.c
@@ -0,0 +1,8 @@
+#include "sys/ecs/pl_entity.h"
+#include "sys/pl_error.h"
+#include "sys/pl_types.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
